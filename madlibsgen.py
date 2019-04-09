e4c7a159504065859c34ce1e6dac53d8dd8a7c2f
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-10 07:32:41 +0800

    median

--- madlibsgen.py
+++ madlibsgen.py
@@ -28,17 +28,56 @@ print weekend('July')
 # instead of print countdown(3).
 
 def countdown(n):
-     i = n
-     while i > 0:
-        print i
-        i -=1
+     while n > 0:
+        print n
+        n -=1
      print 'Blastoff!'
 
-
-    
 
 countdown(3)
 #>>> 3
 #>>> 2
 #>>> 1
 #>>> Blastoff!
+
+
+
+# Define a procedure, median, that takes three
+# numbers as its inputs, and returns the median
+# of the three numbers.
+
+# Make sure your procedure has a return statement.
+
+def bigger(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def biggest(a,b,c):
+    return bigger(a,bigger(b,c))
+
+def median(a,b,c):   
+    if bigger(a,b) < biggest(a,b,c):
+        return bigger(a,b)
+    return c
+
+
+
+print(median(1,2,3))
+#>>> 2
+
+print(median(9,3,6))
+#>>> 6
+
+print(median(7,8,7))
+#>>> 7
+
+print(median(11,33,22))
+#>>> 22
+
+print(median(4,5,6))
+#>>> 5
+
+print(median(8,7,7))
+#>>> 7
