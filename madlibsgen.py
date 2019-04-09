38c0c0baf9c61bffcc33a551d87e847de708eb2d
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-10 07:46:23 +0800

    random nouns

--- madlibsgen.py
+++ madlibsgen.py
@@ -81,3 +81,23 @@ print(median(4,5,6))
 
 print(median(8,7,7))
 #>>> 7
+
+
+
+# Write code for the function random_noun, which takes in no inputs but outputs 
+# one of two nouns randomly. Use the randint function to generate a number 
+# from 0-1 and return a noun depending on whether the number is equal to 0 or 1. 
+# Feel free to experiment with different nouns, but for submission purposes return 
+# the string "sofa" if the number is 0, else return "llama".
+
+from random import randint
+
+def random_noun():
+    # your code here
+    num = randint(0,1)
+    if num == 0:
+        return 'sofa'
+    if num == 1:
+        return 'llama'
+
+print random_noun()
