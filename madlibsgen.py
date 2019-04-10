57c1aa84b0a183c5ae6aeb58694006ac6dcb6c03
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-10 08:38:53 +0800

    mad lib

--- madlibsgen.py
+++ madlibsgen.py
@@ -119,14 +119,13 @@ def random_verb():
         return 'run'
     return 'kayak'
 
-    
-    
+
 print random_verb()
 
 
-# Write code for the function word_transformer, which takes in a string word as input. 
-# If word is equal to "NOUN", return a random noun, if word is equal to "VERB", 
-# return a random verb, else return the first character of word. 
+# Write code for the function word_transformer, which takes in a string word as input.
+# If word is equal to "NOUN", return a random noun, if word is equal to "VERB",
+# return a random verb, else return the first character of word.
 
 
 def word_transformer(word):
@@ -137,7 +136,40 @@ def word_transformer(word):
         return random_verb()
     return word[0]
 
+
 print word_transformer('NOUN')
 
 
+# Let's put it all together. Write code for the function process_madlib, which takes in
+# a string "madlib" and returns the string "processed", where each instance of
+# "NOUN" is replaced with a random noun and each instance of "VERB" is
+# replaced with a random verb. You're free to change what the random functions
+# return as verbs or nouns for your own fun, but for submissions keep the code the way it is!
+
+def process_madlib(mad_lib):
+    processed = ""
+    box_length = 4
+    # your code here
+    # you may find the built-in len function useful for this quiz
+    # documentation: https://docs.python.org/2/library/functions.html#len
+    i = 0
+    while i < len(mad_lib):
+       frame = mad_lib[i: i + box_length]
+       to_add = word_transformer(frame)
+       processed += to_add
+       if len(to_add) == 1:
+           i += 1
+       else:
+           i+=4
+    return processed
+
+
+    # print length
+
+
+    
+test_string_1 = "This is a good NOUN to use when you VERB your food"
+test_string_2 = "I'm going to VERB to the store and pick up a NOUN or two."
+print process_madlib(test_string_1)
+print process_madlib(test_string_2)
 
